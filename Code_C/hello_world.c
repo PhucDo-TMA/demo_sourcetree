594b11469befe0f710f3849f636ab40ea8f8490a
DESKTOP-DIMRFFE\dohuu <dohuuphuc5678@gmail.com>
2023-03-28 08:38:36 +0700

    accept incoming change

--- Code_C/hello_world.c
+++ Code_C/hello_world.c
@@ -2,10 +2,7 @@
 
 int main() {
    printf("Hello, World!");
-<<<<<<< HEAD
-=======
    printf("Hello, Phuc dep trai!");
->>>>>>> 8c0a240 (modified branch 2)
    return 0;
    /*add cmt change here*/
 }
