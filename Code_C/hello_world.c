2257ce35021e6a50e34aa28a87c31b9ce021ce69
DESKTOP-DIMRFFE\dohuu <dohuuphuc5678@gmail.com>
2023-08-28 21:42:39 +0700

    main: commit sau khi squash 1 va 2

--- Code_C/hello_world.c
+++ Code_C/hello_world.c
@@ -4,6 +4,8 @@ int main() {
    printf("Hello, World!");
    printf("Hello, Phuc dep trai!");
    printf("Hello, Phuc dep trai nhat!");
+
+   printf("Hello, Phúc đẹp trai và có vợ đẹp nhất!");
    return 0;
    /*add cmt change here*/
 }
