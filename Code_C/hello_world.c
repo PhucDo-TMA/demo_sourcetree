9d4b2d34c3108babf264a30de29c61d16e23c333
Do Huu Phuc <dohuuphuc5678@gmail.com>
2023-09-05 21:52:22 +0700

    main: after cherry pick

--- Code_C/hello_world.c
+++ Code_C/hello_world.c
@@ -6,7 +6,7 @@ int main() {
    printf("Hello, Phuc dep trai nhat!");
 
    printf("Hello, Phúc đẹp trai và có vợ đẹp nhất!");
-   printf("hello main");
+   printf("hello Branch_3");
    return 0;
    /*add cmt change here*/
 }
